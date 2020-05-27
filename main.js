29966efc0154381c7af8c85cb9f9aa88747e5260
mablancoalvarez <mablancoalvarez@gmail.com>
2020-05-27 14:11:14 +0200

    add photos

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 
 
-function playSound (e) {
+function playSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
     if (!audio) return;
@@ -14,11 +14,9 @@ function playSound (e) {
 function removeTransition(e) {
     if (e.propertyName !== 'transform') return;
     this.classList.remove('playing');
-   
+
 }
 const keys = document.querySelectorAll('.key');
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-
-
 
 window.addEventListener('keydown', playSound);
